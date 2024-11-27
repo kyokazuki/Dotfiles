49723cf8aa31665d5105b9b2144042cb329b1004
KyoKazuki <kyokazuki@proton.me>
2024-11-27 16:20:39 +0900

    fixed autostart

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# mouse and display settings
+
+# mouse and keyboard settings
 unclutter -idle 2 &
-xrandr --output HDMI-A-0 --mode 1920x1080 --rate 144
 xset r rate 250 40
 setxkbmap -option caps:swapescape
 xinput set-prop '8' 'libinput Accel Speed' -0.8
