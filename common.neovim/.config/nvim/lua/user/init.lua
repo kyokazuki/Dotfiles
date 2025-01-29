0340441a5cb4534c4a13e6debc8b5eadf4b84ed8
KyoKazuki <kyokazuki@proton.me>
2025-01-30 00:54:27 +0900

    added gitignore to ignore neovim plugins, changed neovim package name

--- common.neovim/.config/nvim/lua/user/init.lua
+++ common.neovim/.config/nvim/lua/user/init.lua
@@ -6,17 +6,19 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
-Plug('nvim-tree/nvim-web-devicons')
-Plug('nvim-tree/nvim-tree.lua')
 Plug('nvim-treesitter/nvim-treesitter', { ['do'] = function()
     vim.fn[':TSUpdate']()
 end })
 Plug('numToStr/Comment.nvim')
+Plug('nvim-tree/nvim-web-devicons')
+Plug('nvim-tree/nvim-tree.lua')
+Plug('mbbill/undotree')
 Plug('navarasu/onedark.nvim')
 
 vim.call('plug#end')
 
-require("user.plugins.nvim-tree")
 require("user.plugins.treesitter")
 require("user.plugins.comment")
+require("user.plugins.nvim-tree")
+require("user.plugins.undotree")
 require("user.plugins.colorscheme")
